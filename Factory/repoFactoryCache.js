2770cbdd8efbe111384f3820161cc6f5c2b5bbad
ShawnTseng <shawntseng40@gmail.com>
2020-03-02 13:22:11 +0800

    add: 優化程式碼

--- Factory/repoFactoryCache.js
+++ Factory/repoFactoryCache.js
@@ -1,23 +1,14 @@
 var repoFactory = function() {
-  this.getRepo = function(repoType) {
-    if (repoType === "task") {
-      if (this.taskRepo) {
-        console.log("Retrieving from chche");
-        return this.taskRepo;
-      } else {
-        this.taskRepo = require("./task-repository")();
-        return this.taskRepo;
-      }
-    }
-    if (repoType === "user") {
-      var userRepo = require("./user-repository")();
-      return userRepo;
-    }
-    if (repoType === "project") {
-      var projectRepo = require("./project-repository")();
-      return projectRepo;
-    }
-  };
+  var repos = this;
+  var repoList = [
+    { name: "task", source: "./task-repository" },
+    { name: "user", source: "./user-repository" },
+    { name: "project", source: "./project-repository" }
+  ];
+
+  repoList.forEach(function(repo) {
+    repos[repo.name] = require(repo.source)();
+  });
 };
 
 module.exports = new repoFactory();
