5836b8b8e2ca9fca8eea639c685701bda1049933
ShawnTseng <shawntseng40@gmail.com>
2020-03-05 09:46:10 +0800

    add: three services for demo Observer Pattern

--- Observer/main.js
+++ Observer/main.js
@@ -1,5 +1,26 @@
 var Task = require("./task");
 
+var notificationService = function() {
+  var message = "Notifying ";
+  this.update = function(task) {
+    console.log(`${message}+${task.user} for task ${task.name}`);
+  };
+};
+
+var loggingService = function() {
+  var message = "Logging ";
+  this.update = function(task) {
+    console.log(`${message}+${task.user} for task ${task.name}`);
+  };
+};
+
+var auditingService = function() {
+  var message = "Auditing ";
+  this.update = function(task) {
+    console.log(`${message}+${task.user} for task ${task.name}`);
+  };
+};
+
 var task1 = new Task({ name: "create a demo fro constructors", user: "Shawn" });
 
-task1.save();;
+task1.save();
