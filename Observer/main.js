ab96695dab307c5c83fb89dab605c582d2948544
ShawnTseng <shawntseng40@gmail.com>
2020-03-05 10:30:22 +0800

    add: Observer Pattern

--- Observer/main.js
+++ Observer/main.js
@@ -3,21 +3,21 @@ var Task = require("./task");
 var notificationService = function() {
   var message = "Notifying ";
   this.update = function(task) {
-    console.log(`${message}+${task.user} for task ${task.name}`);
+    console.log(`${message} ${task.user} for task ${task.name}`);
   };
 };
 
 var loggingService = function() {
   var message = "Logging ";
   this.update = function(task) {
-    console.log(`${message}+${task.user} for task ${task.name}`);
+    console.log(`${message} ${task.user} for task ${task.name}`);
   };
 };
 
 var auditingService = function() {
   var message = "Auditing ";
   this.update = function(task) {
-    console.log(`${message}+${task.user} for task ${task.name}`);
+    console.log(`${message} ${task.user} for task ${task.name}`);
   };
 };
 
@@ -35,6 +35,42 @@ ObserverList.prototype.get = function(index) {
   }
 };
 
-var task1 = new Task({ name: "create a demo fro constructors", user: "Shawn" });
+ObserverList.prototype.count = function() {
+  return this.observerList.length;
+};
+
+var ObservableTask = function(data) {
+  Task.call(this, data);
+  this.observers = new ObserverList();
+};
+
+ObservableTask.prototype.addObserver = function(observer) {
+  this.observers.add(observer);
+};
+
+ObservableTask.prototype.notify = function(context) {
+  var observerCount = this.observers.count();
+  for (let i = 0; i < observerCount; i++) {
+    this.observers.get(i)(context);
+  }
+};
+
+ObservableTask.prototype.save = function() {
+  this.notify(this);
+  Task.prototype.save.call(this);
+};
+
+var task1 = new ObservableTask({
+  name: "create a demo for constructors",
+  user: "Shawn"
+});
+
+var not = new notificationService();
+var ls = new loggingService();
+var audit = new auditingService();
+
+task1.addObserver(not.update);
+task1.addObserver(ls.update);
+task1.addObserver(audit.update);
 
 task1.save();
