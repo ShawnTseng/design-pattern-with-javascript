aeb95137804513c76f2a54fa8127611e21bc609a
ShawnTseng <shawntseng40@gmail.com>
2020-03-05 11:28:59 +0800

    add: implement Mediator Pattern

--- Mediator/main.js
+++ Mediator/main.js
@@ -21,6 +21,40 @@ var auditingService = function() {
   };
 };
 
+var mediator = (function() {
+  var channels = {};
+
+  var subscribe = function(channel, context, func) {
+    if (!channels[channel]) {
+      channels[channel] = [];
+    }
+
+    channels[channel].push({
+      context: context,
+      func: func
+    });
+  };
+
+  var publish = function(channel) {
+    if (!channels[channel]) {
+      return false;
+    }
+
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    for (let i = 0; i < channels[channel].length; i++) {
+      const subscription = channels[channel][i];
+      subscription.func.apply(subscription.context, args);
+    }
+  };
+
+  return {
+    channels: {},
+    subscribe: subscribe,
+    publish: publish
+  };
+})();
+
 var task1 = new Task({
   name: "create a demo for mediators",
   user: "Shawn"
@@ -30,4 +64,13 @@ var not = new notificationService();
 var ls = new loggingService();
 var audit = new auditingService();
 
-task1.save();
+mediator.subscribe("complete", not, not.update);
+mediator.subscribe("complete", ls, ls.update);
+mediator.subscribe("complete", audit, audit.update);
+
+task1.complete = function() {
+  mediator.publish("complete", this);
+  Task.prototype.complete.call(this);
+};
+
+task1.complete();
